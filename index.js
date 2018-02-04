d2327eb488d2e222affbfd9aa2b58cd014ce71ed
Nicolas Marini <nmarini44@gmail.com>
2018-02-04 17:41:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeathlesPlay (musicians, instruments) {
-  var array = [] 
+  const array = [] 
   for (let i = 0, l = musicians.length; i < l; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
